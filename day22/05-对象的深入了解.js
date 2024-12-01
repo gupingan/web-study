5028e9d8f32f4d10a64e0df2fe920184ce537c13
gupingan <gupingan@proton.me>
2024-12-02 00:25:06 +0800

    update day22

---
+++ day22/05-对象的深入了解.js
@@ -0,0 +1,191 @@
+// Object 是所有对象的父级对象
+// 对于 Object 构造函数，如果传入的是：
+// 1. null或者undefined 返回 空对象
+let obj1 = new Object(null)
+let obj2 = new Object(undefined)
+console.log(obj1, obj2)
+// 2. 基本类型的值，则会构造其包装类型的对象
+let obj3 = new Object(100)
+console.log(obj3)
+// 3. 引用类型的值，则会返回这值本身（同引用）
+let obj4 = new Object(obj3)
+console.log(obj4)
+console.log(obj3 === obj4)
+
+// 属性描述符
+// 用于描述对象的值，控制其行为，是否可写可读可修改可枚举等的内部数据结构。
+// 每个属性都有自己对应的属性描述符，保存该属性的元信息
+// 两种形式：数据描述符和存储描述符
+// 数据描述符：具有属性的值，值可写不确定
+// 1.value          属性对应的值，默认为 undefined
+// 2.writable       value是否可被赋值运算符改变，默认为 false
+// 3.configurable   属性描述符是否可以被改变，默认为 false
+// 4.enumerable     该属性是否能出现在对象的枚举属性中，默认为 false
+// 存储描述符：由getter和setter函数对描述的属性
+// 1.get            给属性提供 getter 的方法，如果没有getter则为undefined。属性被访问时执行该方法，无参数传入，但传入this对象
+// 2.set            给属性提供 setter 的方法，如果没有setter则为undefined。属性值被修改时执行该方法，传入新的属性值
+// 3.configurable   属性描述符是否可以被改变，默认为 false
+// 4.enumerable     仅当属性的值为 true，该属性才能出现在对象的枚举属性中，默认为 false
+
+// 获取属性描述符
+// Object.getOwnPropertyDescriptor(obj, prop)
+let obj5 = {
+  name: 'zhangsan',
+  age: 18,
+}
+let obj5Result = Object.getOwnPropertyDescriptor(obj5, 'name')
+console.log(obj5Result)
+/* 
+{
+  value: 'zhangsan',
+  writable: true,
+  enumerable: true,
+  configurable: true
+}
+*/
+
+// 设置数据描述符
+// 定义至少一个属性：Object.defineProperties(obj, props)
+let obj6 = {
+  name: 'zhangsan',
+  gender: 'male',
+}
+Object.defineProperties(obj6, {
+  name: {
+    value: '张三',
+    configurable: true,
+    enumerable: false,
+    /*
+    循环遍历语句遍历对象的属性有三种方式，如下所示，enumerable 影响下方的代码：
+    1. for...in 语句：遍历该对象的可枚举属性
+    2. Object.keys() 方法：该方法返回一个数组，该数组包含了该对象中所有自有可枚举的属性名称。
+    3. Object.getOwnPropertyNames() 方法：该方法返回一个数组，该数组包含了该对象中所有属性名称。
+    */
+  },
+  age: {
+    // 新创建的属性，它的属性描述符将采用默认值，而不是 true
+    value: 32,
+  },
+})
+console.log(Object.getOwnPropertyDescriptor(obj6, 'name'))
+console.log(Object.getOwnPropertyDescriptor(obj6, 'age'))
+for (const key in obj6) {
+  console.log(obj6[key]) // male
+}
+console.log(obj6) // { gender: 'male' }，其它属性不可枚举的原因
+
+// 定义单个属性：Object.defineProperty(obj, prop, desc)
+Object.defineProperty(obj6, 'hobby', {
+  value: '吃饭睡觉打飞机',
+  enumerable: true,
+})
+
+Object.defineProperty(obj6, 'sayHi', {
+  value: function () {
+    console.log(this.name + ': hi!')
+  },
+  enumerable: true,
+})
+obj6['hobby'] = '睡觉吃饭打虫虫' // 更改无效
+console.log(obj6) // { gender: 'male', hobby: '吃饭睡觉打飞机', sayHi: [Function: value] }
+obj6.sayHi() // 张三: hi!
+
+// 设置存储描述符
+// get()获取属性值，无参数，this 指向当前对象，this中不可访问当前设置的属性
+// set()修改属性值，接受唯一参数新值
+let hStr = '江南 Style'
+let obj7 = {}
+Object.defineProperty(obj7, 'name', {
+  // value: hStr,
+  // writable: true,
+  // 不能同时设置数据描述符和存储描述符，互斥的，会导致 TypeError
+  get: function () {
+    return hStr
+  },
+  set: function (nV) {
+    hStr = nV
+  },
+})
+console.log(obj7.name)  // 江南 Style
+obj7.name = '西风'
+console.log(obj7.name)  // 西风
+// 其实还能这样写
+let obj8 = {
+  get name() {
+    return hStr
+  },
+  set name(newValue) {
+    hStr = newValue
+  }
+}
+console.log(obj8.name) // 西风
+obj8.name = '玛卡巴卡'
+console.log(obj8.name)  // 玛卡巴卡
+ 
+
+// 防篡改对象
+// ES5 中，新增了放置防篡改对象的属性或方法的机制，提供了三种保护方式：
+// 1. 禁止扩展  禁止为对象新增属性或方法
+// 2. 密封对象  禁止新增属性或方法，禁止配置现有的属性或方法的描述符(writable 除外)，仅允许读写属性的值
+// 3. 冻结对象  禁止对对象执行任何修改的操作
+// 一旦定义防篡改对象，无法撤销，无法改回
+
+// 1. 禁止扩展  preventExtensions  禁止增，可改删，可配置所有  静默失败或者异常
+console.log('-------------禁止扩展------------')
+let obj9 = {name : '黑寡妇'}
+Object.preventExtensions(obj9)
+obj9.age = 18
+console.log(obj9)  // { name: '黑寡妇' } 说明不可添加
+obj9.name = '美羊羊'
+console.log(obj9)  // { name: '美羊羊' } 说明可修改
+Object.defineProperty(obj9, 'name', {enumerable: false})
+console.log(obj9) // {} 说明可配置 enumerable
+console.log(obj9.name)  // 美羊羊
+delete obj9.name
+console.log(obj9.name)  // undefined 说明可删除
+// Object.defineProperty(obj9, 'age', {value: 18})  // 异常：TypeError: Cannot define property age, object is not extensible
+// 可通过 Object.isExtensible(obj9) 来判断对象是否可扩展
+console.log(Object.isExtensible(obj9)) // false
+
+// 2. 密封对象  seal  禁止增、删，可改，可配置writable  静默失败或者异常
+console.log('-------------密封对象------------')
+let obj10 = {name : '黑寡妇'}
+Object.seal(obj10)
+obj10.age = 18
+console.log(obj10)  // { name: '黑寡妇' } 说明不可添加
+// Object.defineProperty(obj10, 'name', {
+//   enumerable: false
+// }) // 异常：TypeError: Cannot redefine property: name
+// console.log(obj10)  // 说明不可配置 enumerable
+obj10.name = '阿里嘎多美羊羊桑'
+console.log(obj10)  // { name: '阿里嘎多美羊羊桑' } 说明可读写
+delete obj10.name
+console.log(obj10.name)  // 阿里嘎多美羊羊桑，说明不可删除
+Object.defineProperty(obj10, 'name', {
+  writable: false
+})
+console.log(Object.getOwnPropertyDescriptor(obj10, 'name'))  // 说明可配置 writable
+// 可通过 Object.isSealed(obj10) 来判断对象是否密封
+console.log(Object.isSealed(obj10))  // true
+
+// 3. 冻结对象  freeze  禁止对对象执行任何修改的操作  静默失败或异常
+console.log('-------------冻结对象------------')
+let obj11 = {name : '黑寡妇'}
+Object.freeze(obj11)
+obj11.age = 18
+console.log(obj11)  // { name: '黑寡妇' } 说明不可添加
+obj11.name = '最佳舔狗沸羊羊桑'
+console.log(obj11)  // { name: '黑寡妇' } 说明不可读写
+// Object.defineProperty(obj11, 'name', {
+//   enumerable: false
+// }) // 异常：TypeError: Cannot redefine property: name
+// console.log(obj11)  // 说明不可配置 enumerable
+// Object.defineProperty(obj11, 'name', {
+//   writable: true
+// }) // 异常：TypeError: Cannot redefine property: name
+// console.log(obj11)  // 说明不可配置 writable 为 true
+delete obj11.name
+console.log(obj11.name)  // 黑寡妇，说明不可删除
+// 可通过 Object.isFrozen(obj10) 来判断对象是否冻结
+console.log(Object.isFrozen(obj11))  // true
+
---
+++ day22/06-继承与原型链.js
@@ -0,0 +1,213 @@
+// 继承是指将特性从父代传递给子代，以便新代码可以重用并基于现有代码的特性进行构建
+
+// JavaScript 使用对象实现继承。每个对象都有一条链接到另一个称作原型的对象的内部链。
+// 该原型对象有自己的原型，依此类推，直到原型是 null 的对象
+// null 没有原型
+
+// 运行时可修改原型链的任何成员，可换掉原型
+
+// JavaScript 继承的实现方式 是 原型机制，而 Python Java 等语言的继承实现方式则是类机制
+
+// 继承属性
+// JavaScript 对象有一条指向原型对象的链。当试图访问对象的属性时，不仅在该对象上查找属性，
+// 还会在该对象的原型上查找属性，以及原型的原型，依此类推，直到找到一个名字匹配的属性或到达原型链的末尾
+const o1 = {
+  a: 1,
+  b: 2,
+  __proto__: {
+    b: 3,
+    c: 4,
+  },
+}
+console.log(o1.a) // 1 自有属性
+console.log(o1.b) // 2 自有属性
+console.log(o1.c) // 4 原型属性
+console.log(o1.d) // undefined 没有找到
+// 查找属性
+console.log(o1.hasOwnProperty('c')) // false
+console.log(Object.hasOwn(o1, 'c')) // false
+console.log(Object.getPrototypeOf(o1).hasOwnProperty('c') ) // true
+console.log('c' in o1) // true
+console.log(Object.keys(o1)) // [ 'a', 'b' ]
+console.log(o1.__proto__) // { b: 3, c: 4 }
+console.log(Object.getPrototypeOf(o1)) // { b: 3, c: 4 }
+console.log(Object.getPrototypeOf(o1) === o1.__proto__) // true
+// 给对象设置属性会创建自有属性。获取和设置行为规则的唯一例外是当它被 getter 或 setter 拦截时
+Object.defineProperty(o1, 'b', {
+  get: function () {
+    return this.__proto__.b
+  },
+})
+console.log(o1.b) // 3 从原型继承
+// 原型链可以创建更长
+console.log(o1.__proto__.__proto__) // [Object: null prototype] {}
+console.log(o1.__proto__.__proto__.__proto__) // null
+
+// 继承方法
+const parent = {
+  value: 2,
+  method() {
+    return this.value++
+  },
+}
+
+console.log(parent.method()) // 2
+
+const child = {
+  __proto__: parent,
+}
+console.log('---------第一轮--------')
+console.log(child) // {}
+console.log(child.method()) // 3
+console.log(child.value) // 4
+console.log(child.__proto__.value) // 3
+console.log(parent.value) // 3
+// child.method() this 指向 child，child 继承的是 parent 的方法
+// 在 child 上寻找属性 value，没有找到，会在其原型上找到，即 parent.value（3）
+// 最后先返回 3，紧接着自增1，此时 child.value = 3 + 1 = 4
+console.log('---------第二轮--------')
+console.log(child) // { value: 4 }
+console.log(child.method()) // 4
+console.log(child.value) // 5
+console.log(child.__proto__.value) // 3
+console.log(parent.value) // 3
+// 此时 child 拥有 value 属性，所以 this.value 指向 child.value(4)，而不是 parent.value(3)
+
+// 原型的强大之处在于，如果一组属性应该出现在每一个实例上，那我们就可以复用它们——尤其是对于方法
+// const boxes = [
+//   { value: 1, getValue() { return this.value; } },
+//   { value: 2, getValue() { return this.value; } },
+//   { value: 3, getValue() { return this.value; } },
+// ]
+// 这是不优雅的，函数是相同的，只是 this 指向不同，冗余且不必要的
+// 原型改造
+// const boxPrototype = {
+//   getValue() {
+//     return this.value
+//   },
+// }
+
+// const boxes = [
+//   { __proto__: boxPrototype, value: 1 },
+//   { __proto__: boxPrototype, value: 2 },
+//   { __proto__: boxPrototype, value: 3 },
+// ]
+// 降低了内存使用率，但是为每个对象手动绑定 __proto__ 很麻烦，可以使用构造函数
+function Box(value) {
+  this.value = value
+}
+// 通过构造函数创建的每一个实例都会自动将构造函数的 prototype 属性作为其 [[Prototype]]
+Box.prototype.getValue = function () {
+  return this.value
+}
+const boxes = [new Box(1), new Box(2), new Box(3)]
+// box --> Box.prototype --> Object.prototype --> null
+console.log('验证开始000')
+console.log(boxes[0].__proto__ === Box.prototype)
+console.log(Box.prototype.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
+console.log('验证结束000')
+
+
+// 创建实例，实例的原型将用 prototype，实例继承父类，其属性是__proto__
+// Constructor.prototype 默认具有一个自有属性：constructor，引用构造函数本身
+console.log(Box.prototype.constructor === Box)
+// 这允许我们在任何实例中访问原始构造函数
+console.log(new Box(1).__proto__.constructor === Box)
+
+// 用类
+class NewBox {
+  constructor(value) {
+    this.value = value
+  }
+  // 在 NewBox.prototype 上创建方法
+  getValue() {
+    return this.value
+  }
+}
+let box1 = new NewBox(1)
+console.log(box1.getValue())  // 1
+NewBox.prototype.getValue = function () {
+  return this.value + 1
+}
+console.log(box1.getValue())  // 2
+console.log(Object.getPrototypeOf(NewBox) === Function.prototype) // true
+
+
+class Rectangle {
+  constructor(height, width) {
+    this.name = "Rectangle";
+    this.height = height;
+    this.width = width;
+  }
+}
+
+class FilledRectangle extends Rectangle {
+  constructor(height, width, color) {
+    super(height, width);
+    this.name = "Filled rectangle";
+    this.color = color;
+  }
+}
+
+const filledRectangle = new FilledRectangle(5, 10, "blue");
+// filledRectangle ---> FilledRectangle.prototype ---> Rectangle.prototype ---> Object.prototype ---> null
+console.log('验证开始111')
+console.log(filledRectangle.__proto__ === FilledRectangle.prototype)
+console.log(FilledRectangle.prototype.__proto__ === Rectangle.prototype)
+console.log(Rectangle.prototype.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
+console.log('验证结束111')
+
+
+
+// 通过 Object.create() 方法，该对象的 [[Prototype]] 是该函数的第一个参数
+const a = { a: 1 };
+// a ---> Object.prototype ---> null
+const b = Object.create(a);
+// b ---> a ---> Object.prototype ---> null
+console.log(b.a); // 1（继承的）
+const c = Object.create(b);
+// c ---> b ---> a ---> Object.prototype ---> null
+const d = Object.create(null);
+// d ---> null（d 是一个直接以 null 为原型的对象）
+console.log(d.hasOwnProperty);
+// undefined，因为 d 没有继承 Object.prototype
+// c -> b -> a -> Object.prototype -> null
+console.log('验证开始222')
+console.log(c.__proto__ === b)
+console.log(b.__proto__ === a)
+console.log(a.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
+console.log('验证结束222')
+
+// Object.setPrototypeOf() 允许修改现有对象的 [[Prototype]] 内部属性
+// 能强制为 Object.create(null) 创建的无原型的对象设置原型
+const rawObj = { a: 1 };
+const anotherObj = { b: 2 };
+Object.setPrototypeOf(rawObj, anotherObj)
+console.log(rawObj.b)  // 2
+// rawObj ---> anotherObj ---> Object.prototype ---> null
+console.log('验证开始333')
+console.log(rawObj.__proto__ === anotherObj)
+console.log(anotherObj.__proto__ === Object.prototype)
+console.log(Object.prototype.__proto__ === null)
+console.log('验证结束333')
+
+
+const noWell = {};
+// 请不要使用该方法：仅作为示例。
+noWell.__proto__ = { barProp: "bar val" };
+noWell.__proto__.__proto__ = { fooProp: "foo val" };
+console.log(noWell.fooProp);
+console.log(noWell.barProp);
+
+// 通常建议使用 ES6 中的 Reflect.getPrototypeOf() 或者 Object.getPrototypeOf()
+// Reflect.setPrototypeOf() 或者 Object.setPrototypeOf()
+
+// 原型污染：攻击者通过某种手段修改 JavaScript 对象的原型。
+Object.prototype.toString = function () {  return console.log('桀桀桀') }
+console.log({}.toString())  // 桀桀桀 undefined
+// 1.使用 Object.create(null)， 方法创建一个原型为 null 的新对象，这样无论对 原型做怎样的扩展都不会生效
+// 2.使用 Object.freeze(obj) 冻结指定对象，使之不能被修改属性，成为不可扩展对象
+// 3.建立 JSON schema ，在解析用户输入内容时，通过 JSON schema 过滤敏感键名
